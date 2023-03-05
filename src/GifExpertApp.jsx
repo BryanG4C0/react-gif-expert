272e14f21c884bb08bbf3333c8e7e4947a1ec0a8
Bryan Garcia <ing.bryangaco@gmail.com>
2023-03-05 17:16:18 -0500

    Pintando en jsx  mock array de categorias en GiftExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
+
+/*
+cuando queremos almacenar información y cambiar el html, inmediatamente debemos pensar que necesitamos algun HOOK para mantener el estado
+ */
 
 export const GifExpertApp = () => {
+
+    
+    // Siempre tratar de inicializar el estado
+    const [categories, setCategories] = useState(['One punch ', 'Dragon ball']);
+
+    console.log(categories);
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <ol>
+                {categories.map((category, index) => {
+                    return (
+                    <li key={index}>
+                        {category}
+                    </li>
+                    );
+                })}
+            </ol>
         </>
     )
 }
