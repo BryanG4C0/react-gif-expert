c6c99d48484403f88fde7685f79db4e5d638a86e
Bryan Garcia <ing.bryangaco@gmail.com>
2023-03-05 17:24:25 -0500

    Agregar un categoria quemada, ejercicio para cambiar valor de una propiedad array en el estado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,9 +4,15 @@ import { useState } from 'react'
 cuando queremos almacenar información y cambiar el html, inmediatamente debemos pensar que necesitamos algun HOOK para mantener el estado
  */
 
+
 export const GifExpertApp = () => {
 
-    
+    const onAddCategory = () => {
+        const newCategory = 'Valorant'
+        //setCategories([...categories, newCategory]);
+        setCategories( cat=> [...cat, newCategory]);
+    };
+
     // Siempre tratar de inicializar el estado
     const [categories, setCategories] = useState(['One punch ', 'Dragon ball']);
 
@@ -18,12 +24,13 @@ export const GifExpertApp = () => {
             {/* Input */}
 
             {/* Listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map((category, index) => {
                     return (
-                    <li key={index}>
-                        {category}
-                    </li>
+                        <li key={index}>
+                            {category}
+                        </li>
                     );
                 })}
             </ol>
