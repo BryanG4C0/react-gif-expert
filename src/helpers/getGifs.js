3099877db5d5e4f8a4710d65670af0c2c54f638f
Bryan Garcia <ing.bryangaco@gmail.com>
2023-03-05 20:36:44 -0500

    Ajuste en la url de consulta de los gifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/trending?api_key=PiSCIjh7zEtaJo9dXvjZFrgkpaXfK3Ep&q=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=PiSCIjh7zEtaJo9dXvjZFrgkpaXfK3Ep&q=${category}&limit=10`
     const response = await fetch(url);
 
     const { data } = await response.json();
