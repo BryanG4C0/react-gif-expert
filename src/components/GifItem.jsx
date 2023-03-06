e3e4f612e63f71f105f5a33bea873393c9cc541a
Bryan Garcia <ing.bryangaco@gmail.com>
2023-03-05 20:38:27 -0500

    Creación custom hook, consulta de imagenes useFetchGifs.js

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,6 +1,5 @@
 
-export const GifItem = ({ title, url }) => {
-    console.log(title, url);
+export const GifItem = ({ title, url }) => {    
     return (
         <div className="card">
             <img src={url} alt={title} />
