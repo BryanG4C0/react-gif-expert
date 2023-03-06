1d03a8e6e243e04a5c1ecadda83a254d2dca003e
Bryan Garcia <ing.bryangaco@gmail.com>
2023-03-05 19:34:50 -0500

    Implementación HOOK use effect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 
 export const GifGrid = ({ category }) => {
 
+    const [counter, setCounter] = useState(10);
+
+    useEffect(() => {
+        getGifs(category);
+    }, []);
 
     // No se debe colocar la ejecución de una función directamente en un componente por que se va a ejecutar n cantidad de veces
-    getGifs(category);
+    //getGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
             <p>Hola Mundo</p>
+            <h5>{counter}</h5>
+            <button onClick={() => { setCounter(counter + 1) }}>+1</button>
             {/* {
                 gifs.map((gif) => (
                     <p>{gif}</p>
